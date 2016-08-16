3877818e6c6a167e69fa3613d6d8ae706cd678aa
U-CARESOURCE\jfcarr <jfcarr@4000337L.CareSource.corp>
2016-08-16 09:46:17 -0400

    Informational messages for each set of tests.

--- test.c
+++ test.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include "lib.h"
 
+void HeaderMessage (char *message)
+{
+	printf ("\n__%s__\n", message);
+}
+
 int main()
 {
+	HeaderMessage("Simple Printing");
 	SayHello();
 
 	SayGoodbye();
 
+	HeaderMessage("Returning Numbers");
 	printf("Result of ReturnInt is %d\n", ReturnInt());
 
 	printf("Result of ReturnFloat is %f\n", ReturnFloat());
 
+	HeaderMessage("Statics");
 	SetStatic(10);
 	printf ("Static value is %d\n", GetStatic());
 	SetStatic(20);
 	printf ("Static value is %d\n", GetStatic());
 
+	HeaderMessage("Pointer References");
 	char testString[25];
 	sprintf (testString, "Old Value");
 	printf ("String is %s\n", testString);
