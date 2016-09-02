877e3d37a87eb10a4f6b9e03ca0736a9392abac4
U-CARESOURCE\jfcarr <jfcarr@4000337L.CareSource.corp>
2016-09-02 12:26:15 -0400

    Moved C library client test code into separate function.

--- ptest.py
+++ ptest.py
@@ -6,37 +6,41 @@ from ctypes import *
 def HeaderMessage(message):
 	print ("\n___" + message + "___")
 
-try:
-	libc = cdll.LoadLibrary("./libshared.so")
+def LibCTest():
+	try:
+		libc = cdll.LoadLibrary("./libshared.so")
 
-except Exception as ex:
-	print ("[LoadLibrary Error] " + str(ex))
-	sys.exit()
+	except Exception as ex:
+		print ("[LoadLibrary Error] " + str(ex))
+		sys.exit()
 
-try:
-	HeaderMessage("Simple Printing")
-	libc.SayHello()
-	libc.SayGoodbye()
-	
-	HeaderMessage("Returning Numbers")
-	returnInt = libc.ReturnInt()
-	print ("Returned int is " + str(returnInt))
-	
-	libc.ReturnFloat.restype = c_float
-	returnFloat = libc.ReturnFloat()
-	print ("Returned float is " + str(returnFloat))
-	
-	HeaderMessage("Statics")
-	libc.SetStatic(10)
-	print ("Static value is " + str(libc.GetStatic()))
-	libc.SetStatic(20)
-	print ("Static value is " + str(libc.GetStatic()))
-	
-	HeaderMessage("Pointer References")
-	myString = c_char_p("Original Value")
-	print ("String is " + myString.value)
-	libc.PointerReference(myString)
-	print ("String is now " + myString.value)
-	
-except Exception as ex:
-	print ("[libc Calling Error] " + str(ex))
+	try:
+		HeaderMessage("___ C Test ___")
+		HeaderMessage("Simple Printing")
+		libc.SayHello()
+		libc.SayGoodbye()
+		
+		HeaderMessage("Returning Numbers")
+		returnInt = libc.ReturnInt()
+		print ("Returned int is " + str(returnInt))
+		
+		libc.ReturnFloat.restype = c_float
+		returnFloat = libc.ReturnFloat()
+		print ("Returned float is " + str(returnFloat))
+		
+		HeaderMessage("Statics")
+		libc.SetStatic(10)
+		print ("Static value is " + str(libc.GetStatic()))
+		libc.SetStatic(20)
+		print ("Static value is " + str(libc.GetStatic()))
+		
+		HeaderMessage("Pointer References (NOT working)")
+		returnString = "Old Value";
+		print ("String is " + returnString)
+		libc.PointerReference(returnString)
+		print ("String is now " + returnString)
+		
+	except Exception as ex:
+		print ("[libc Calling Error] " + str(ex))
+
+LibCTest()
