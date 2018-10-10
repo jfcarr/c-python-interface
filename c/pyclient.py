cb8d125faf1451c1b2f7ee670ab7246d21ec7696
Jim Carr <hidden-email>
2018-10-10 16:21:17 -0400

    Cleaned up print formatting.

--- c/pyclient.py
+++ c/pyclient.py
@@ -4,7 +4,7 @@ import sys
 from ctypes import *
 
 def HeaderMessage(message):
-	print ("\n___" + message + "___")
+	print ("\n___{0}___".format(message))
 
 def LibCTest():
 	HeaderMessage("___ C Test ___")
@@ -13,7 +13,7 @@ def LibCTest():
 		libc = cdll.LoadLibrary("./libshared.so")
 
 	except Exception as ex:
-		print ("[C LoadLibrary Error] " + str(ex))
+		print ("[C LoadLibrary Error] {0}".format(str(ex)))
 		sys.exit()
 
 	try:
@@ -23,23 +23,23 @@ def LibCTest():
 		
 		HeaderMessage("Returning Numbers")
 		returnInt = libc.ReturnInt()
-		print ("Returned int is " + str(returnInt))
+		print ("Returned int is {0}".format(str(returnInt)))
 		
 		libc.ReturnFloat.restype = c_float
 		returnFloat = libc.ReturnFloat()
-		print ("Returned float is " + str(returnFloat))
+		print ("Returned float is {0}".format(str(returnFloat)))
 		
 		HeaderMessage("Statics")
 		libc.SetStatic(10)
-		print ("Static value is " + str(libc.GetStatic()))
+		print ("Static value is {0}".format(str(libc.GetStatic())))
 		libc.SetStatic(20)
-		print ("Static value is " + str(libc.GetStatic()))
+		print ("Static value is {0}".format(str(libc.GetStatic())))
 		
 		HeaderMessage("Pointer References (NOT working)")
 		returnString = "Old Value";
-		print ("String is " + returnString)
+		print ("String is {0}".format(returnString))
 		libc.PointerReference(returnString)
-		print ("String is now " + returnString)
+		print ("String is now {0}".format(returnString))
 		
 	except Exception as ex:
 		print ("[libc Calling Error] " + str(ex))
